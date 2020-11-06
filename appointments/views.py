f794ee1ea638624ec8cb05f125421d5baf7699dc
LauraRincon <lcrinconr@gmail.com>
2020-11-05 22:46:23 -0600

    Change check view
    Displays projects for a specific user

--- appointments/views.py
+++ appointments/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 
-from .models import Project
+from .models import User, Project
 from .forms import ProjForm
 
 
@@ -42,38 +42,49 @@ def new(request):
         )
 
 
-def check(request, pk=None):
-    if pk:
+def check(request, pk=None, id=None):
+    if id:
         try:
-            proj = Project.objects.get(pk=pk)
-        except Project.DoesNotExist:
-            raise Http404('Project with pk {} doesn\'t exist'.format(pk))
-        return render(
-            request,
-            'check.html',
-            {
-                'object_pk': proj.pk,
-                'object_name': proj.name,
-                'object_place': proj.place,
-                'object_billing': proj.billing,
-                'object_category': proj.category,
-                'object_appointment': proj.appt_date,
-            }
-        )
+            user = User.objects.get(pk=id)
+            if pk:
+                try:
+                    proj = Project.objects.get(user=user, pk=pk)
+                    return render(
+                        request,
+                        'check.html',
+                        {
+                            'object_pk': proj.pk,
+                            'object_name': proj.name,
+                            'object_place': proj.place,
+                            'object_billing': proj.billing,
+                            'object_category': proj.category,
+                            'object_appointment': proj.appt_date,
+                        }
+                    )
+                except Project.DoesNotExist:
+                    raise Http404('Project with pk {} doesn\'t exist'.format(pk))
+            else:
+                proj_dict = {}
+                for proj in user.project_set.all():
+                    proj_dict[proj.name] = {
+                        'pk': proj.pk,
+                        'place': proj.place,
+                        'billing': proj.billing,
+                        'category': proj.category,
+                        'appt': proj.appt_date,
+                        'uid' : id
+                    }
+                return render(
+                request,
+                'check.html',
+                {
+                    'proj_dict': proj_dict,
+                }
+                )
+        except User.DoesNotExist:
+            raise Http404('User with id {} doesn\'t exist'.format(id))
     else:
-        proj_dict = {}
-        for proj in Project.objects.all():
-            proj_dict[proj.name] = {
-                'pk': proj.pk,
-                'place': proj.place,
-                'billing': proj.billing,
-                'category': proj.category,
-                'appt': proj.appt_date,
-            }
         return render(
             request,
-            'check.html',
-            {
-                'proj_dict': proj_dict,
-            }
+            'check.html'
         )
